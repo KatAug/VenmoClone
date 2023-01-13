3019aaa79eeb51ca4b5fea2010775a9efd126cb1
Katrina Augustine <katrinaaugustine2022@gmail.com>
2023-01-13 12:26:02 -0600

    Started project

---
+++ main.py
@@ -0,0 +1,6 @@
+#2 dictionaries one called user_one and the other called user_two
+
+user_one = {"full_name":"Donald Duck", "user_name": "", "password": "", "account_balance": "integer", "connected_banks": }
+
+user_two = {"full_name":"Mickey Mouse", "user_name": "", "password": "", "account_balance": "", "connected_banks": ""}
+
