b9f572afa7a3ce2f713a01bbce02d411cdc13914
Katrina Augustine <katrinaaugustine2022@gmail.com>
2023-01-13 14:25:07 -0600

    Added user_one and user_two dictionaries

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 #2 dictionaries one called user_one and the other called user_two
 
-user_one = {"full_name":"Donald Duck", "user_name": "", "password": "", "account_balance": "integer", "connected_banks": }
+user_one = {
+    'full_name':'SpongeBob Squarepants', 
+    'user_name':'krusty krab', 
+    'password':'pineapple', 
+    'account_balance': '750000', 
+    'connected_banks': [
+        ("RBFCU", 10000), 
+        ("Pioneer Bank", 25000), 
+        ("Wells Fargo", 100000)
+    ] 
+}
 
-user_two = {"full_name":"Mickey Mouse", "user_name": "", "password": "", "account_balance": "", "connected_banks": ""}
+user_two = {
+    'full_name':'Sandy Cheeks', 
+    'user_name':'treedome', 
+    'password':'karate', 
+    'account_balance':'900000', 
+    'connected_banks': [
+    ("First Federal National Bank", 50000), 
+    ("Chase", 19000)
+    ("Guaranty Bank & Trust", 250000),
+    ]
+    
+ }
 
