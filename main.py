b72d220b67ba45e281eaec27be7a3af383ac4b4c
Katrina Augustine <katrinaaugustine2022@gmail.com>
2023-01-17 11:14:31 -0600

    Almost there. Need to fix one more issue.

--- main.py
+++ main.py
@@ -42,25 +42,25 @@ banks = bank_one, bank_two, bank_three
 for x, y in banks:
     print(f"{str(x)}: ${str(y)}")
 
-user_prompt = input(f"Would you like to proceed to transfer money to {user_two['full_name']}? ")
-while user_prompt == "y":
-    user_prompt = input("How much would you like to transfer? ")
-    if user_prompt > user_one['account_balance']:
-            print("You have insufficent funds. Try another amount.")
-            user_prompt = input("How much would you like to transfer? ")
-    if user_prompt < user_one['account_balance']: 
-        print(f"${user_prompt} will be transferred to {user_two['full_name']}.")
-        new_acct_bal_one = ((int)(user_one['account_balance']) - (int)(user_prompt))
-        print(new_acct_bal_one)
-        new_bal = new_acct_bal_one - (int)(user_prompt) #How do I get this to loop? 
-        print(new_bal)
-        #new_acct_bal_two = ((int)(user_prompt)(int)(user_two['account_balance']) + (int)(user_prompt))
-        user_prompt = input("Would you like to make another transfer? ") 
-    else: 
-        print(f"Balance: {(int)(user_one['account_balance'])} ")
-        print("That will end this transaction. Have a great day!")  #Need these two lines to print when saying no after several transfers.     
-if user_prompt == "n":
-            #print(f"Balance: {(int)(user_one['account_balance'])} ")
-            print("That will end this transaction. Have a great day!") #Need this to print only when saying no to transferring to Sandy Cheeks.
-
-#Need to be able to print the correct balance after making transfers. 
+transfer = input(f"Would you like to transfer money to {user_two['full_name']}? ")
+while transfer == "y":
+    amount = input("How much would you like to transfer? ")#Why is it looping up to here when done? 
+    if amount > user_one['account_balance']:
+        print("You have insufficent funds. Try another amount.")
+        print(input("How much would you like to transfer? "))
+    if amount < user_one['account_balance']:
+        print(f"${amount} will be transferred to {user_two['full_name']}.")
+        print((int)(amount) + (int)(user_two['account_balance']))
+        x = (int)(user_one['account_balance'])
+        y = (int)(amount)
+        new_acct_bal = x-y
+        current_bal = new_acct_bal - y
+        user_prompt = input("Would you like to make another transfer? ")
+    if user_prompt == "n":
+        print(f"Balance: ${current_bal}")
+        print("That will end this transaction. Have a great day!")
+if transfer == "n":
+    print("That will end this transaction. Have a great day!")
+else:
+    print("That will end this transaction. Have a great day!")
+ 
