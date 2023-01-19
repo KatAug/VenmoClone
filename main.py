a1d79854ccf6564d61d29dc8a07971f560a62f8b
Katrina Augustine <katrinaaugustine2022@gmail.com>
2023-01-18 18:07:08 -0600

    Made the Venmo project look more pleasing on the eyes!

--- main.py
+++ main.py
@@ -23,37 +23,47 @@ user_two = {
     ]
     
  }
-
+print()
+print(f"Welcome, {(user_one['full_name'])}!")
+print()
 user_name_prompt = input("Please enter your user name: ")
 while user_name_prompt != (user_one['user_name']):
     print("User name is incorrect! Please enter the correct user name.")
     user_name_prompt = input("Please enter your user name: ")
 password_prompt = input("Please enter your password: ")
+print()
 while password_prompt != (user_one['password']):
     print("Your password is incorrect! Please enter the correct password.")
     password_prompt = input("Please enter your password: ")
 if password_prompt == (user_one['password']):
     print(f"You're current balance is ${user_one['account_balance']}")
-
+print()
+print("The available funds from your connected accounts are as follows:")
+print()
 bank_one = user_one['connected_banks'][0]
 bank_two = user_one['connected_banks'][1]
 bank_three = user_one['connected_banks'][2]
 banks = bank_one, bank_two, bank_three
 for x, y in banks:
     print(f"{str(x)}: ${str(y)}")
-
+print()
 transfer = input(f"Would you like to transfer money to {user_two['full_name']}? ")
+print()
 while transfer == "y":
     amount = int(input("How much would you like to transfer? "))
+    print()
     if amount > user_one['account_balance']:
         print("You have insufficent funds. Try another amount.")    
     elif amount < user_one['account_balance']:
-        print(f"${amount} will be transferred to {user_two['full_name']}.")
+        print(f"${amount} will be transferred to {user_two['full_name']}")
+        print()
         user_one['account_balance'] -= amount
         amount += user_two['account_balance']
         transfer = input(f"Would you like to make another transfer to {user_two['full_name']}? ")
+        print()
         if transfer == "n":
-            print(f"Balance: ${ user_one['account_balance']}")
+            print(f"Your remaining balance is ${ user_one['account_balance']}")
+            print()
 else:
     print("That will end this transaction. Have a great day!")
 
